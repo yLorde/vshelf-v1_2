e8d6076ff6e6533571004b07390cb419ac7d2c26
yLorde <davisouza2204@gmail.com>
2025-09-10 08:07:57 -0300

    1.2.11

--- menus/paginaInicial.js
+++ menus/paginaInicial.js
@@ -15,12 +15,12 @@ module.exports = function paginaInicial(mainWindow) {
                     mainWindow.loadFile('index.html')
                 },
             },
-            {
-                label: 'Abrir Configurações',
-                click() {
-                    mainWindow.loadFile('pages/app/config.html');
-                }
-            },
+            // {
+            //     label: 'Abrir Configurações',
+            //     click() {
+            //         mainWindow.loadFile('pages/app/config.html');
+            //     }
+            // },
             {
                 label: 'Exibição',
                 submenu: [
--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -1,11 +1,5 @@
 (function () {
-    if (window.localStorage.getItem('tema') === 'escuro') {
-        document.documentElement.style.setProperty('--background-color', '#191919');
-        document.documentElement.style.setProperty('--default-bg', '#121212');
-        document.documentElement.style.setProperty('--default-text', '#fff');
-    } else {
-        document.documentElement.style.setProperty('--background-color', '#ccc');
-        document.documentElement.style.setProperty('--default-bg', '#fff');
-        document.documentElement.style.setProperty('--default-text', '#000');
-    };
+    document.documentElement.style.setProperty('--background-color', '#000');
+    document.documentElement.style.setProperty('--default-bg', '#121212');
+    document.documentElement.style.setProperty('--default-text', '#fff');
 })()
