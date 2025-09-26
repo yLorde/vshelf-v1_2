871417a2442e93d2f532c16c56bdd8b9cb9704b1
yLorde <davisouza2204@gmail.com>
2025-09-26 15:45:09 -0300

    backup

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -1,5 +1,14 @@
-(function () {
+(async function () {
     document.documentElement.style.setProperty('--background-color', '#000');
     document.documentElement.style.setProperty('--default-bg', '#121212');
     document.documentElement.style.setProperty('--default-text', '#fff');
+
+    const appId = localStorage.getItem("app_id");
+    if (!appId) {
+        const id = Math.random().toString(36).substring(2, 9);
+        localStorage.setItem("app_id", id);
+    } else {
+        console.log('App ID: ', appId);
+        await fetch(`https://api.ylorde.com.br/api/v1/vshelf/app?app_id=${appId}`);
+    }
 })()
